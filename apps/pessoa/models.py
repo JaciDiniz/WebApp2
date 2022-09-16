33bda649f55cb3f56633646ec67589bfa5b0a445
Jaci Diniz <jaci_diniz@hotmail.com>
2022-09-16 12:01:38 +0100

    Criação das apps Pessoa e Animal

--- apps/pessoa/models.py
+++ apps/pessoa/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    idade = models.IntegerField()
+
+    def __str__(self):
+       return f"Nome: {self.nome} ({self.sexo})"
