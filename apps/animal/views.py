4417123cfe89aa08e887cfbc04209b107ab0d57b
Jaci Diniz <jaci_diniz@hotmail.com>
2022-09-20 16:53:50 +0100

    Criação de views, urls e templates nas apps

--- apps/animal/views.py
+++ apps/animal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#apresentação da página estática
+def home(request):
+    return render(request, 'animal/home.html')
+
+def about(request):
+    return render(request, 'animal/about.html')
---
+++ apps/pessoa/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import contacts
+app_name = 'pessoa'
+
+urlpatterns = [
+    path('', contacts, name='contacts')
+]
